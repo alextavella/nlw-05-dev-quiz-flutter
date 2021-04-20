9631c81d683445254727e269805d4bad4c52522e
alextavella <eu.alextavella@gmail.com>
2021-04-19 22:31:07 -0300

    first commit

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,41 @@
+import 'package:devquiz/home/home_page.dart';
+// import 'package:devquiz/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: HomePage(),
+    );
+  }
+}
+
+// class AppWidget extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'DevQuiz',
+//       home: Scaffold(
+//         appBar: AppBar(title: Text("NLW5 Flutter")),
+//         body: Center(
+//           child: Container(
+//             decoration: BoxDecoration(
+//               color: Colors.purple,
+//               borderRadius: BorderRadius.circular(10),
+//             ),
+//             child: Text("Olá, Alex Tavella",
+//               style: TextStyle(fontSize: 30),
+//             ),
+//           )
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: (){},
+//           child: Icon(Icons.add)
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:devquiz/core/core.dart';
+
+void main() {
+  runApp(AppWidget());
+}
